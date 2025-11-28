5ae9fb8d9ccac32f9c505ceda0ab5db553c2c0f9
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-11-28 22:48:16 +0100

    guns noredir

---
+++ api/guns-noredir.js
@@ -0,0 +1,9 @@
+// /MrTomiCZ/mrtomicz.eu/api/ip.js
+export default async function handler(req, res) {
+    const params = new URLSearchParams(req.url.search);
+    const site = params.get('site');
+    const html = await fetch(site);
+    const parsed = await html.text();
+
+    res.status(200).setHeader("Content-Type", "text/html").send(parsed || "No content");
+}
