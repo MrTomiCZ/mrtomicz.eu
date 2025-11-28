9af554a3c6cbb797e66ea2a19ddca0692a7ef926
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-11-28 22:20:37 +0100

    raw

--- api/ip.js
+++ api/ip.js
@@ -1,6 +1,7 @@
 // /MrTomiCZ/mrtomicz.eu/api/ip.js
 export default function handler(req, res) {
     const raw = req.headers['x-forwarded-for'] || req.headers['x-real-ip'] || '';
+    console.log(raw);
     let ip = '';
 
     if (Array.isArray(raw)) ip = raw[0];
