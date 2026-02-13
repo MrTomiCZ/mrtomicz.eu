6cf5f2ba83765e808a040914e93ae00d6cd8d0b3
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2026-02-13 21:25:51 +0100

    OHH

--- js/inject.js
+++ js/inject.js
@@ -1,10 +1,10 @@
 function loadScripts(scripts) {
     scripts.forEach(sc => {
-        console.log("loading script "+sc[0])
+        console.log("loading script "+sc)
         var script = document.createElement("script");
         script.type = "text/javascript";
-        script.src = sc[0];
-        script.onload = sc[1] || "";
+        script.src = sc/*[0]*/;
+        //script.onload = sc[1] || "";
         document.head.appendChild(script);
     });
 }
