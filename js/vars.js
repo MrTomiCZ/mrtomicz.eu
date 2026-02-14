8daf8b9aa64e57d3ad303254ee5f9cf91f2767c2
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2026-02-14 14:40:37 +0100

    uh il do i t later

--- js/vars.js
+++ js/vars.js
@@ -1,1 +1,7 @@
+const navbar = document.createElement("div");
+navbar.classList.add("navb");
+navbar.innerHTML = `
+navbar indev
+`;
+//document.body.prepend
 document.getElementById("footer").innerHTML = "<p>&copy; 2026 MrTomiCZ. All rights reserved.</p>";
