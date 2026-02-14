9abd1604976398bb42dd88279a22a3304d99c14d
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2026-02-14 18:06:48 +0100

    Added fix of footer, yes

--- js/vars.js
+++ js/vars.js
@@ -4,4 +4,12 @@ navbar indev
 `;
 navbar.id = "navb";
 document.body.prepend(navbar);
-document.getElementById("footer").innerHTML = "<p>&copy; 2026 MrTomiCZ. All rights reserved.</p>";
+
+const footer = document.createElement("div");
+footer.innerHTML = `<div id="footer"><p>&copy; 2026 MrTomiCZ. All rights reserved.</p></div>`;
+footer.style.display = "flex";
+footer.style.margin = 0;
+footer.style.flexDirection = "column";
+footer.style.flex = 1;
+document.body.appendChild(footer);
+//document.getElementById("footer").innerHTML = "<p>&copy; 2026 MrTomiCZ. All rights reserved.</p>";
